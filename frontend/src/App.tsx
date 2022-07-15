a92df8e79de9f9fc9e2ecd273e4b0568b00b2d44
yuri-silveiraa <yuri.pires.silveira@gmail.com>
2022-07-15 19:42:33 -0300

    First componet

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
+import NotificationButton from "./components/NotificationButton"
 
 
 function App() {
   return (
+    <>
     <h1>Olá mundo!</h1>
+    <NotificationButton />
+    <NotificationButton />
+    </>
   )
 }
 
